181f54e32b0a310859e58ecaa96f1c4bbd0d1486
ThesllaDev <theslladev@gmail.com>
2024-08-18 21:08:01 -0300

    ✨🍱💄Feat: create ui of intro section

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,13 @@
 export default function Home() {
   return (
-    <main></main>
+    <main>
+      <header className="mx-4 my-8 break-all text-center">
+        <h1 className="main-title-logo text-[2rem]">ThesllaDev</h1>
+        <h2 className="secondary-title-logo mt-2 text-[2rem]">
+          Desenvolvedor Front-end
+        </h2>
+        <hr className="mx-auto h-[3px] max-w-3xl bg-red text-red blur-[1px]" />
+      </header>
+    </main>
   );
 }
